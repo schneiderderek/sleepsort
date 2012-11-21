b390a73fda198c93dc64780e350e8a0ceeaa2a6c
marchtemp <march.temp@gmail.com>
2012-11-21 05:00:00 -0500

    scratch version

---
+++ SleepSort.java
@@ -0,0 +1,44 @@
+import java.util.Random; 
+
+class sleepEl extends Thread
+{
+	private int wait; 
+	public sleepEl(int wait)
+	{
+		this.wait = wait; 
+	}
+	public void run() 
+	{
+		try
+		{
+			sleep(10*wait);
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println("Something went wrong");
+		}
+		System.out.println(wait);
+	}
+}
+
+public class SleepSort
+{
+	public static void main (String [] args)
+	{
+		int[] arr = new int[30] ;
+		Random rand = new Random()	;
+
+		for(int a=0; a<arr.length; a++)
+		{
+			arr[a] = rand.nextInt(100) ;
+		}
+		printSorted(arr);
+	}
+	public static void printSorted(int[] marray)
+	{
+		for(int a=0; a<marray.length; a++)
+		{
+			new sleepEl(marray[a]).start();
+		}
+	}
+}
